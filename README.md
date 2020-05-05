5ab1c2c4d7805b9520ff17d834ffb84fc2536eef
DreamLi <18829346477@163.com>
2020-05-05 22:50:01 +0800

    update.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # javafamily-rest
 JavaFamily Rest Interfaces
+
+## Implementation
+
+* Create a class to extends `org.javafamily.rest.BaseQueryEngine` or implement `org.javafamily.rest.QueryEngine`
+* add `baseUrl` of rest api
+* call `org.javafamily.rest.QueryEngine.query` method getting result.
+
--- src/main/java/org/javafamily/rest/QueryEngine.java
+++ src/main/java/org/javafamily/rest/QueryEngine.java
@@ -4,7 +4,7 @@
 
 package org.javafamily.rest;
 
-import org.apache.http.*;
+import org.apache.http.HttpEntity;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -14,9 +14,6 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.javafamily.model.HttpHeaders;
 import org.javafamily.model.HttpParameters;
-
-import java.util.Collections;
-import java.util.List;
 
 public interface QueryEngine {
 
@@ -43,14 +40,14 @@ public interface QueryEngine {
    default String query(String api, HttpParameters params, HttpHeaders headers) throws Exception {
       URIBuilder uriBuilder = new URIBuilder(baseUrl() + api);
 
-      uriBuilder.addParameters(requiredParameters());
+      uriBuilder.addParameters(requiredParameters().build());
       uriBuilder.addParameters(params.build());
 
       HttpGet httpGet = new HttpGet(uriBuilder.build());
 
       httpGet.setConfig(getRequestConfig());
 
-      requiredHeaders().stream().forEach(httpGet::addHeader);
+      requiredHeaders().build().stream().forEach(httpGet::addHeader);
       headers.build().stream().forEach(httpGet::addHeader);
 
       CloseableHttpClient httpClient = HttpClients.createDefault();
@@ -62,12 +59,12 @@ public interface QueryEngine {
       return EntityUtils.toString(entity, charset());
    }
 
-   default List<NameValuePair> requiredParameters() {
-      return Collections.emptyList();
+   default HttpParameters requiredParameters() {
+      return new HttpParameters();
    }
 
-   default List<Header> requiredHeaders() {
-      return Collections.emptyList();
+   default HttpHeaders requiredHeaders() {
+      return new HttpHeaders();
    }
 
    default String charset() {
